43d229e3758b9885c03e8be0cbb5599c26ae0907
Tawalla Fentress <tawallaf@yahoo.com>
2019-03-19 18:57:07 -0400

    Clean up and corrections

--- Lab2_TF/Lab2/Person.cs
+++ Lab2_TF/Lab2/Person.cs
@@ -7,28 +7,23 @@ namespace Lab2
         public string FirstName;
         public string LastName;
         public int Age;
+        public double AverageAges;
         public Person Spouse;
         public bool isMarried;
         public string isMarriedString;
-        public static double SumofAllAges;
-        public static int Count { get; private set; }
-        public static int SumOfAllAges { get; private set; }
-
-        public static double AverageAge()
-        {
-            return (double)SumOfAllAges / (double)Count;
-        }
-
+        public static double SumofAllAges;         
+        public static int AverageAge { get; private set; }
+       
         public string GetFullName()
         {
             return this.FirstName + " " + this.LastName;
         }
 
-        public static void AllPersonAge(int age)
+        public double GetAverageAge()
         {
-            Count++;
-            SumOfAllAges += age;
+            return SumofAllAges / 4;
         }
+       
         public void PrintNameAndAge()
         {
             System.Console.WriteLine("My name is " + this.GetFullName() + ".");
@@ -40,7 +35,7 @@ namespace Lab2
         public static void PrintPersonCountAndAverageAge()
         {
             System.Console.WriteLine(" | Number of People : 4".PadRight(36) + " | ");
-            System.Console.WriteLine((" | Average Age      : " + AverageAge().ToString()).PadRight(36) + " | ");
+            System.Console.WriteLine(" | Average Age      :   " + GetAverageAge() +   "."     );
         }
     }    
 }
--- Lab2_TF/Lab2/Program.cs
+++ Lab2_TF/Lab2/Program.cs
@@ -6,8 +6,9 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Person firstPerson = new Person();
+            // Person 1 information
 
+            Person firstPerson = new Person();
             System.Console.WriteLine("Please enter your First Name: ");
             firstPerson.FirstName = System.Console.ReadLine();
             System.Console.WriteLine("Please enter your Last Name: ");
@@ -16,24 +17,28 @@ namespace Lab2
             firstPerson.Age = int.Parse(System.Console.ReadLine());
             firstPerson.Spouse = new Person();
 
+            // Person 1 Martial Status and spouse information
 
             System.Console.WriteLine("Are you married?     ");
-            string isMarriedString = System.Console.ReadLine().ToLower();   // Added for marital status
+            string isMarriedString = System.Console.ReadLine().ToLower();
             bool isMarried = isMarriedString.Contains("y");
-
             System.Console.WriteLine("What is your Spouse's Name?");
             firstPerson.Spouse.FirstName = System.Console.ReadLine();
             System.Console.WriteLine("What is your Spouse's Age?");
             firstPerson.Spouse.Age = int.Parse(System.Console.ReadLine());
             firstPerson.Spouse.Spouse = firstPerson;
 
-            Person.SumofAllAges = Person.SumofAllAges + firstPerson.Age;
-
             firstPerson.PrintNameAndAge();
             firstPerson.Spouse.PrintNameAndAge();
 
-            Person secondPerson = new Person();
+            // Sum of Person 2 and Spouse Age
 
+            Person.SumofAllAges = firstPerson.Age + firstPerson.Spouse.Age;
+
+
+            // Person 2 information
+
+            Person secondPerson = new Person();
             System.Console.WriteLine("Please enter your First Name: ");
             secondPerson.FirstName = System.Console.ReadLine();
             System.Console.WriteLine("Please enter your Last Name: ");
@@ -42,34 +47,35 @@ namespace Lab2
             secondPerson.Age = int.Parse(System.Console.ReadLine());
             secondPerson.Spouse = new Person();
 
+            // Person 2 Martial Status and information
 
             System.Console.WriteLine("Are you married?     ");
             string isMarriedString2 = System.Console.ReadLine().ToLower();
             bool isMarried2 = isMarriedString.Contains("y");
-
             System.Console.WriteLine("What is your Spouse's Name?");
             secondPerson.Spouse.FirstName = System.Console.ReadLine();
             System.Console.WriteLine("What is your Spouse's Age?");
             secondPerson.Spouse.Age = int.Parse(System.Console.ReadLine());
             secondPerson.Spouse.Spouse = secondPerson;
 
-            Person.SumofAllAges = Person.SumofAllAges + secondPerson.Age;
+            // Sum of Person 2 and Spouse Age
+
+            Person.SumofAllAges = secondPerson.Age + secondPerson.Spouse.Age;
+
 
             secondPerson.PrintNameAndAge();
             secondPerson.Spouse.PrintNameAndAge();
 
-            Person.PrintPersonCountAndAverageAge();
-            
-
+           
             System.Console.WriteLine("Press any key to continue...");
             System.Console.ReadLine();
-
+           }
 
         }
 
        
     }
-        }
+        
     
 
 
